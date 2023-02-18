1801d471274dfa68895ff9f72f0f7b55ebd4bd39
Christy G. Hanson <34938344+ChristyGHanson@users.noreply.github.com>
2023-02-18 03:03:35 -0700

    minor edits

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-# JavaScript Password Generator
+# Christy's Random Password Generator
 
 - Screenshot: ![alt text](images/app-screenshot.jpg) 
 - GitHub Repo URL: https://github.com/ChristyGHanson/JS-Password-Generator
 - Deployed App URL:
+
+## Description
+Create an application that generates a random password based on user-selected criteria. 
+This app will run in the browser and feature dynamically updated HTML and CSS powered by your JavaScript code. 
+It will also feature a clean and polished user interface and be responsive, ensuring that it adapts to multiple screen sizes.
+
+# How to Use Christy's Random Password Generator
+
+1. Click the button that reads "Generate Password."
+2. Type in a number between 8 and 128. This is your password's length. Click OK.
+3. Click OK if you would like lowercase letters, uppercase letters, special characters, and numbers.
+4. The password will randomly generate and appear in the text box.
+
+# Technology Used
+
+In addition to HTML, CSS, and JavaScript, we used the following technology for this website.
+
+* [Font Awesome](https://fontawesome.com/) provides a collection of free icons and toolkits (you have to make a free account). 
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,73 @@
 // Assignment code here
 
-
 // Get references to the #generate element
-// variable called generateBtn
-// document accesses index.html file
 var generateBtn = document.querySelector("#generate");
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharacter = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword(); //generate password has not been defined yet.
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+// Prompts happen inside the function.
+function generatePassword() {
+  let pwLength = prompt("How long do you want your password to be? (Between 8 and 128 characters.)");
+  if (pwLength < 8 || pwLength > 128 || isNaN(pwLength)) {
+    alert('Password must be a number between 8 and 128 characters.');
+    return; //return; stops function.
+  }
+  console.log(pwLength);
+  let useLower = confirm('Do you want to use lowercase letters?'); // test for false. then return.
+  let useUpper = confirm('Do you want to use uppercase letters?');
+  let useSpecialChar = confirm('Do you want to use special characters?');
+  let useNum = confirm('Do you want to use numbers?');
+  console.log(useLower, useUpper, useSpecialChar, useNum); //pass more than one thing into a function.
+  //evaluate characters they chose to add. 
+  // Whatever the user selected, we will use to generate a password. Leave array completely empty.
+  let useChar = [];
+  //evaluate. copy this and do it for the rest.
+  if (useLower) {
+    useChar.push(...lowerCase); //push is a method. do this for the other arrays. ... is the spread operator. push one character at a time into each array "space."
+  };
+
+  console.log(useChar); // put the array in the array. not what we want.
+  // test for false cases using pipes, etc.
+  // random number generator. look for useChar. how long character password will be. use a loop.
+  // first pwLength, get a random number, use random number to get a character out of the array.
+  // randomly get a number x amount of times. Use a for loop and a random number generator.
+  let string = '';
+  // Do all of it before the return. 
+  return string;
+}
+
 // Add event listener to generate button
 // eventTarget.addEventListener() method of the EventTarget interface.
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //will cause functions to activate above.
 
+
+
+// Else if allows you to test more than one condition
+// The first condition is false, so the second condition is evaluated. Logs "Cost Rating: $$""
+
+
+
+// Temporary comment:
+// 
+// Objects are a collection of values. This is from Challenge lesson 3.
+//  separate values with a comma.
+// var cars = {
+  // make: "Honda",
+  // model: "Civic",
+  // year: "2003",
+// };
+
+// var displayList = function () {
+  // alert("Make: " + cars.make);
+// };
+
+// displayList();
